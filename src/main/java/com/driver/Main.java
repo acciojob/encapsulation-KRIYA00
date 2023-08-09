40c2971e59ec15c6df5000a542688846fa7c0e51
ysi <yshouldi@gmail.com>
2023-08-09 22:09:25 +0530

    attempt1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly ob = new RWOnly();
+        ob.setVar(9);
+        System.out.println(ob.getVar());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int var;
+
+    public int getVar() {
+        return var;
+    }
+
+    public void setVar(int var) {
+        this.var = var;
+    }
 }
